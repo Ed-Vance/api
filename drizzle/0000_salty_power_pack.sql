c3dbb5f11f11a1def782b2cc0b8f39ea8ca982c6
MWBruce <MaxwellWallaceBruce@gmail.com>
2024-12-15 21:11:18 +1030

    Reinitialising database

--- drizzle/0000_salty_power_pack.sql
+++ drizzle/0000_salty_power_pack.sql
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"first_name" text,
 	"last_name" text,
 	"email" text,
-	"password" text,
+	"password" text NOT NULL,
 	"phone" text,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
